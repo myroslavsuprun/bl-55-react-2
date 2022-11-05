295dc49ae04d2efcef3daca6b393ead50971fc8d
MiroProger <sirmiroslavsuprun@gmail.com>
2022-11-05 15:40:57 +0200

    Created form

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,7 +4,34 @@ import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
 export class SearchForm extends Component {
+  state = {
+    searchQuery: '',
+  };
+
+  onQueryInputChange = e => {
+    const searchQuery = e.target.value;
+    this.setState({
+      searchQuery,
+    });
+  };
+
+  onFormSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.searchQuery);
+    this.setState({
+      searchQuery: '',
+    });
+  };
+
   render() {
-    return <h2>SearchForm</h2>;
+    const { searchQuery } = this.state;
+    return (
+      <SearchFormStyled onSubmit={this.onFormSubmit}>
+        <InputSearch value={searchQuery} onChange={this.onQueryInputChange} />
+        <FormBtn>
+          <FiSearch />
+        </FormBtn>
+      </SearchFormStyled>
+    );
   }
 }
--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -4,9 +4,22 @@ import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
 export class Gallery extends Component {
+  state = {
+    searchQuery: '',
+  };
+
+  async componentDidUpdate() {}
+
+  onSearchFormSubmit = searchQuery => {
+    this.setState({
+      searchQuery,
+    });
+  };
+
   render() {
     return (
       <>
+        <SearchForm onSubmit={this.onSearchFormSubmit} />
         <Text textAlign="center">Sorry. There are no images ... 😭</Text>
       </>
     );
